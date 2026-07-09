ddc4d43428024f9f04a078e918beb80870574fc2
zhoucheng <zhoucheng133@outlook.com>
2026-07-09 23:01:30 +0800

    迁移到switch

--- src/routes/list_filter.ts
+++ src/routes/list_filter.ts
@@ -1,116 +1,104 @@
-function calculateEpisodesReleased(firstEpisodeTimestamp: number): number {
-  const tmp = new Date();
-  const currentDate = new Date(tmp.getFullYear(), tmp.getMonth(), tmp.getDate());
-  const difference = currentDate.getTime() - firstEpisodeTimestamp;
-  const daysPassed=Math.floor(difference / (1000 * 60 * 60 * 24));
-  const weeksPassed = Math.floor(daysPassed / 7);
-  return Math.max(weeksPassed, 0) + 1;
-}
-
-// 是否在更新
-function onUpudate(time: number, episode: number): boolean{
-  if(time==0){
-    return false;
-  }else if(calculateEpisodesReleased(time)<episode){
-    return true;
-  }
-  return false;
-}
-
+// 【filter】"none", "progress", "onUpdate", "updateDone", "watchDone", "search", "weekday"
+// 【sort】"az", "za", "add_new_old", "add_old_new", "update_first", "update_end"
 export function toSql(filter: string, param: string | undefined, sort: string | undefined): string {
-  if(filter=="none"){
-    return `SELECT * FROM list ORDER BY ROWID DESC LIMIT ? OFFSET ?`;
-  }else if(filter=="progress"){
-    return `SELECT * FROM list
-    WHERE (
-      (time = 0 AND now < episode)
-      OR
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
-      OR
-      (time != 0 AND ((((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode) AND now < episode)
-    )
-    ORDER BY ROWID DESC
-    LIMIT ? OFFSET ? `
-  }else if(filter=="onUpdate"){
-    return `SELECT * FROM list
-    WHERE (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
-    ORDER BY ROWID DESC
-    LIMIT ? OFFSET ? `
-  }else if(filter=="updateDone"){
-    return `SELECT * FROM list
-    WHERE (
-      (time = 0)
-      OR
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode)
-    )
-    ORDER BY ROWID DESC
-    LIMIT ? OFFSET ? `
-  }else if(filter=="watchDone"){
-    return `SELECT * FROM list
-    WHERE (
-      (time = 0 AND now = episode)
-      OR
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode AND now = episode)
-    )
-    ORDER BY ROWID DESC
-    LIMIT ? OFFSET ? `
-  }else if(filter=="search"){
-    return `SELECT * FROM list
-    WHERE title LIKE '%${param}%'
-    ORDER BY ROWID DESC
-    LIMIT ? OFFSET ? `
-  }else if(filter=="weekday"){
-    return `SELECT * FROM list
-    WHERE (
-      strftime('%w', time / 1000, 'unixepoch', 'localtime') = '${param}'
-      AND
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
-    )
-    ORDER BY ROWID DESC
-    LIMIT ? OFFSET ? `
+
+  switch (filter){
+    case "none":
+      return `SELECT * FROM list ORDER BY ROWID DESC LIMIT ? OFFSET ?`;
+    case "progress":
+      return `SELECT * FROM list
+      WHERE (
+        (time = 0 AND now < episode)
+        OR
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
+        OR
+        (time != 0 AND ((((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode) AND now < episode)
+      )
+      ORDER BY ROWID DESC
+      LIMIT ? OFFSET ? `
+    case "onUpdate":
+      return `SELECT * FROM list
+      WHERE (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
+      ORDER BY ROWID DESC
+      LIMIT ? OFFSET ? `
+    case "updateDone":
+      return `SELECT * FROM list
+      WHERE (
+        (time = 0)
+        OR
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode)
+      )
+      ORDER BY ROWID DESC
+      LIMIT ? OFFSET ? `
+    case "watchDone":
+      return `SELECT * FROM list
+      WHERE (
+        (time = 0 AND now = episode)
+        OR
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode AND now = episode)
+      )
+      ORDER BY ROWID DESC
+      LIMIT ? OFFSET ? `
+    case "search":
+      return `SELECT * FROM list
+      WHERE title LIKE '%${param}%'
+      ORDER BY ROWID DESC
+      LIMIT ? OFFSET ? `
+    case "weekday":
+      return `SELECT * FROM list
+      WHERE (
+        strftime('%w', time / 1000, 'unixepoch', 'localtime') = '${param}'
+        AND
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
+      )
+      ORDER BY ROWID DESC
+      LIMIT ? OFFSET ? `
+    default:
+      return "";
   }
-  return "";
 }
 
 export function calCount(filter: string, param: string | undefined) {
-  if(filter=="none"){
-    return `SELECT COUNT(*) as count FROM list`;
-  }else if(filter=="progress"){
-    return `SELECT COUNT(*) as count FROM list
-    WHERE (
-      (time = 0 AND now < episode)
-      OR
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
-      OR
-      (time != 0 AND ((((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode) AND now < episode)
-    )`
-  }else if(filter=="onUpdate"){
-    return `SELECT COUNT(*) as count FROM list
-    WHERE (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)`
-  }else if(filter=="updateDone"){
-    return `SELECT COUNT(*) as count FROM list
-    WHERE (
-      (time = 0)
-      OR
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode)
-    )`
-  }else if(filter=="watchDone"){
-    return `SELECT COUNT(*) as count FROM list
-    WHERE (
-      (time = 0 AND now = episode)
-      OR
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode AND now = episode)
-    )`
-  }else if(filter=="search"){
-    return `SELECT COUNT(*) as count FROM list
-    WHERE title LIKE '%${param}%'`
-  }else if(filter=="weekday"){
-    return `SELECT COUNT(*) as count FROM list
-    WHERE (
-      strftime('%w', time / 1000, 'unixepoch', 'localtime') = '${param}'
-      AND
-      (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
-    )`
+  switch (filter) {
+    case "none":
+      return `SELECT COUNT(*) as count FROM list`;
+    case "progress":
+      return `SELECT COUNT(*) as count FROM list
+      WHERE (
+        (time = 0 AND now < episode)
+        OR
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
+        OR
+        (time != 0 AND ((((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode) AND now < episode)
+      )`;
+    case "onUpdate":
+      return `SELECT COUNT(*) as count FROM list
+      WHERE (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)`;
+    case "updateDone":
+      return `SELECT COUNT(*) as count FROM list
+      WHERE (
+        (time = 0)
+        OR
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode)
+      )`;
+    case "watchDone":
+      return `SELECT COUNT(*) as count FROM list
+      WHERE (
+        (time = 0 AND now = episode)
+        OR
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) >= episode AND now = episode)
+      )`;
+    case "search":
+      return `SELECT COUNT(*) as count FROM list
+      WHERE title LIKE '%${param}%'`;
+    case "weekday":
+      return `SELECT COUNT(*) as count FROM list
+      WHERE (
+        strftime('%w', time / 1000, 'unixepoch', 'localtime') = '${param}'
+        AND
+        (time != 0 AND (((strftime('%s','now') * 1000) - time + 604800000 - 1) / 604800000) < episode)
+      )`;
+    default:
+      return "";
   }
-  return "";
 }
